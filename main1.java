b54d5da7946c49f7b0fa8a7066c27ae6d1d7956b
Bosco Collins <159990001+xphlemy@users.noreply.github.com>
2026-01-17 14:47:53 +0300

    Create main1.java

---
+++ main1.java
@@ -0,0 +1,13 @@
+public class Multi3 implements Runnable
+{
+public void run()
+{
+;System.out.println("thread is running…");
+}
+public static void main(String args[])
+{
+Multi3 m1=new Multi3 ();
+Thread t1=new Thread(m1);
+t1.start();
+}
+}
